0998d476d8cc742b2661eefa6ea24f582dbbb29c
kotlsec <65410132+kotlsec@users.noreply.github.com>
2020-06-18 12:06:26 +0530

    Update my.js

--- my.js
+++ my.js
@@ -1,1 +1,7 @@
-amazon_aws_access_key_id: asdugfibdfiafuanusdniasdgnudgnsdgjunundf
+href="https://go.wepay.com/js/jquery-3.4.1.min.js"
+href="https://go.wepay.com/js/popper.min.js"
+href="https://go.wepay.com/templates/styles.c12e8143.js"
+href="https://go.wepay.com/templates/vendors~codebuild/output/src921846114/src/src/containers/Legal~codebuild/output/src921846114/src/src~df232d7b.ab8ac2b7.js"
+href="https://go.wepay.com/templates/vendors~codebuild/output/src921846114/src/src/containers/Legal~codebuild/output/src921846114/src/src~a54d3676.a572eeea.js"
+href="https://go.wepay.com/templates/vendors~codebuild/output/src921846114/src/src/containers/Legal~codebuild/output/src921846114/src/src~4d106432.5bc37ef1.js
+href="https://go.wepay.com/templates/codebuild/output/src921846114/src/src/pages/home/index.05c2f4af.js
